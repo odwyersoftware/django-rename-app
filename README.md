87e594e017466caa01dfa27624214337b779fee8
Richard O'Dwyer <richard@richard.do>
2020-06-01 10:20:37 +0100

    packaging

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # django-rename-app
 
 A Django Management Command to rename existing Django Applications.
+
+[![Build Status](https://travis-ci.org/ODwyerSoftware/django-rename-app.svg?branch=master)](https://travis-ci.org/ODwyerSoftware/django-rename-app) [![PyPI version](https://badge.fury.io/py/django-rename-app.svg)](https://pypi.org/project/django-rename-app/)
+
+# Installation
+
+`pip install django-rename-app`.
+
+Add to your Django settings.py `INSTALLED_APPS`:
+
+```
+INSTALLED_APPS = [
+    ...
+    'django_rename_app',
+    ...
+]
+```
+
+# Usage
+
+
+`python manage.py rename_app sample experiment`
+
+For full detailed instructions see https://odwyer.software/blog/how-to-rename-an-existing-django-application
+
---
+++ setup.py
@@ -0,0 +1,44 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+from __future__ import absolute_import
+
+import os
+from codecs import open
+
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+with open('README.md', 'r', encoding='utf-8') as rm_file:
+    readme = rm_file.read()
+
+with open('HISTORY.md', 'r', encoding='utf-8') as hist_file:
+    history = hist_file.read()
+
+setup(
+    name='django_rename_app',
+    version='0.1.0.dev0',
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    zip_safe=False,
+    description=readme,
+    author='O\'Dwyer Software',
+    author_email='hello@odwyer.software',
+    url='https://github.com/odwyersoftware/django-rename-app',
+    license='Apache 2.0',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
